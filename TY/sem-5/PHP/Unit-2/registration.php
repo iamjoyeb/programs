afcd47550cc4f0a55fa5f5c89ccced14efffe92d
iamjoyeb <iamjoyeb@gmail.com>
2026-08-05 13:20:07 +0530

    added unit-2 code

---
+++ TY/sem-5/PHP/Unit-2/registration.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+		<title>
+			Registration Form
+		</title>
+	</head>
+		<body>
+			<form>
+				<h3 align='center'>Enter Details in Form</h3>
+					<table align='center' border='1'>
+							<h5>
+								<tr>	<td>	Enter  First Name  </td>  <td> <input type='text' name='First-Name'>     </td></tr>
+								<tr>	<td>	Enter  Second Name </td>  <td> <input type='text' name='Second-Name'>    </td></tr>
+								<tr>	<td>	Enter  User ID     </td>  <td><input type='text' name='user-id'>		 </td> </tr>
+								<tr>	<td>	Enter  Password    </td>  <td> <input type='password' name='Password'>   </td></tr>
+								<tr>	<td>	Enter Description  </td>  <td> 
+																   <textarea rows='5' cols='50' name='Description'>
+																					Enter Description
+																   </textarea> </td></tr>								   
+							</h5>
+					</table>
+			</form>
+		</body>
+</html>
