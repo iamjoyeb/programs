6169f92ed7ce109bedeaf3c4bc63318708f84f41
iamjoyeb <iamjoyeb@gmail.com>
2026-08-16 08:36:08 +0530

    fixed java program

--- TY/sem-5/JAVA/Practice/p9.java
+++ TY/sem-5/JAVA/Practice/p9.java
@@ -1,12 +1,14 @@
-class p9 { 
-    p9() { 
-        System.out.println("Default Constructor"); 
-    } 
-    p9(int a) { 
-        System.out.println("Parameterized Constructor="+a); 
-    } 
-    public static void main(String args[]) { 
-        p9 d1=new p9(); 
-        p9 d2=new p9(100); 
-    } 
+class p9 {
+    p9() {
+        System.out.println("Default Constructor");
+    }
+
+    p9(int a) {
+        System.out.println("Parameterized Constructor=" + a);
+    }
+
+    public static void main(String args[]) {
+        new p9();
+        new p9(200);
+    }
 }
