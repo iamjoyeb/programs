a864f33bbca58563046137f8b901939a6fc2f171
iamjoyeb <iamjoyeb@gmail.com>
2026-08-18 17:22:10 +0530

    fixed program

--- TY/sem-5/JAVA/Unit-3/Assignment-1/Method_overloading_and_overriding/METHOD-OVERLOADING/p2.java
+++ TY/sem-5/JAVA/Unit-3/Assignment-1/Method_overloading_and_overriding/METHOD-OVERLOADING/p2.java
@@ -4,30 +4,12 @@
 		
 import java.util.*;
 
-class fibonacci{
-	int n;
+class series{
 	
-	Scanner sc=new Scanner(System.in);
-	
-	void fibo(){
-		System.out.print("Enter An int Number To perform the Fibonacci Series: ");
-		n=sc.nextInt();
-	}
-	
-	void fibo() {
-		super.fibo();
-		
-		System.out.println(" ------ FIBONACCI Series: ------");
-		
-		for(int i=0;i<=n;i++){
-			System.out.println(i);
-		}
-	}
 }
 
 class p2{
-	public Static void main(String args[]){
-		fibo f=new fibo();
-		f.fibo();
+	public static void main(){
+		
 	}
 }
