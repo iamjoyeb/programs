f9cc29c1bb301497d3013401b04065162cca1e60
iamjoyeb <iamjoyeb@gmail.com>
2026-08-18 17:51:28 +0530

    added java 2 program unit-3

--- TY/sem-5/JAVA/Unit-3/Assignment-1/Method_overloading_and_overriding/METHOD-OVERLOADING/p2.java
+++ TY/sem-5/JAVA/Unit-3/Assignment-1/Method_overloading_and_overriding/METHOD-OVERLOADING/p2.java
@@ -5,11 +5,51 @@
 import java.util.*;
 
 class series{
-	
+        int terms;
+        int a=0;
+        int b=1;
+        Scanner sc=new Scanner(System.in);
+        void fibonacci(){
+              
+                System.out.print("Enter Int Value To run Fibonacci Series: ");
+                terms=sc.nextInt();
+
+                fibonacci(terms);
+        }
+
+        void fibonacci(int terms){
+              System.out.println("------ FIBONACCI SERIES ------");
+                
+              for(int i=1; i<=terms; i++){
+                System.out.println(a +" ");
+
+                int c=a+b;
+                a=b;
+                b=c;
+              }
+        }
 }
 
 class p2{
-	public static void main(){
-		
+	public static void main(String args[]){
+                series f=new series();
+
+                f.fibonacci();
 	}
 }
+
+/*
+OUTPUT:
+        Enter Int Value To run Fibonacci Series: 10
+                ------ FIBONACCI SERIES ------
+                        0 
+                        1 
+                        1 
+                        2 
+                        3 
+                        5 
+                        8 
+                        13 
+                        21 
+                        34 
+*/
