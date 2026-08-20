2460c4832cd0ec7afcdb09c18b806e7afd91a6de
iamjoyeb <iamjoyeb@gmail.com>
2026-08-20 21:59:51 +0530

    added java abstraction code

--- TY/sem-5/JAVA/Unit-3/Assignment-1/ABSTRACT-METHOD/p5.java
+++ TY/sem-5/JAVA/Unit-3/Assignment-1/ABSTRACT-METHOD/p5.java
@@ -1,1 +1,28 @@
 /* WRITE A JAVA PROGRAM TO DEMONSTARTE THE USE OF ABSTRACT CLASS */
+
+abstract class abstractdemo{
+    abstract void ad();
+
+    void display(){
+        System.out.println("Abstract Class Demo: ");
+    }
+}
+
+class abstractdemo1 extends abstractdemo{
+    void ad(){
+        System.out.println("Abstract Class Demo: Call From Abstractdemo 1 class : ");
+    }
+}
+class p5{
+    public static void main(String args[]){
+        abstractdemo1 a=new abstractdemo1();
+        a.display();
+        a.ad();
+    }
+}
+
+/*
+    OUTPUT:
+    Abstract Class Demo: 
+    Abstract Class Demo: Call From Abstractdemo 1 class : 
+*/
