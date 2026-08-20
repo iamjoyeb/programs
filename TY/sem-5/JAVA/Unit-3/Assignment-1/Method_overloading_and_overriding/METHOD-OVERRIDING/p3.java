52d91426654710cb055f1ed209c51e1cfeff49cf
iamjoyeb <iamjoyeb@gmail.com>
2026-08-20 16:49:57 +0530

    added java Assignment program of overriding

--- TY/sem-5/JAVA/Unit-3/Assignment-1/Method_overloading_and_overriding/METHOD-OVERRIDING/p3.java
+++ TY/sem-5/JAVA/Unit-3/Assignment-1/Method_overloading_and_overriding/METHOD-OVERRIDING/p3.java
@@ -1,1 +1,25 @@
 /* WRITE A JAVA PROGRAM TO DEMONSTARTIC THE USE OF THE METHOD OVERRIDING IN PROGRA, */
+
+class override{
+    void orr(){
+        System.out.println("Call From Override class orr method");
+    }
+}
+
+class overridesecond extends override{
+    void orr(){
+        System.out.println("Call From overridesecond class orr method");
+    }
+}
+
+class p3{
+    public static void main(String args[]){
+        overridesecond o=new overridesecond();
+
+        o.orr();
+    }
+}
+/*
+    OUTPUT:
+    Call From overridesecond class orr method
+*/
--- TY/sem-5/JAVA/Unit-3/Assignment-1/Method_overloading_and_overriding/METHOD-OVERRIDING/p4.java
+++ TY/sem-5/JAVA/Unit-3/Assignment-1/Method_overloading_and_overriding/METHOD-OVERRIDING/p4.java
@@ -1,1 +1,26 @@
 /*WRITE A JAVA PROGRAM TO DEMONSTRATE THE  SUPER KEYWORD */
+
+class overidesuper{
+    void orrs(){
+        System.out.println("Call From Overidingsuper class method using the super keyword");
+    }
+}
+
+class overrides extends overidesuper{
+    void orrs(){
+        super.orrs();
+        System.out.println("Call From Overidings class method");
+    }
+}
+class p4{
+    public static void main(String args[]){
+        overrides orrs=new overrides();
+
+        orrs.orrs();
+    }
+}
+/*
+    OUTPUT:
+    Call From Overidingsuper class method using the super keyword
+    Call From Overidings class method
+*/
